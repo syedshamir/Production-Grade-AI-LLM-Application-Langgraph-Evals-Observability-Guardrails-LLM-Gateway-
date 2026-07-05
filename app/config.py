cc662ead40db626ae6afa7d8579aa2886bb9daa6
syedshamir <shamirraza44@gmail.com>
2026-07-05 16:25:48 +0300

    Embeddings

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Settings:
     TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY")
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
+    GROQ_FALLBACK_API_KEY: str = os.getenv("GROQ_FALLBACK_API_KEY")
     GROQ_MODEL = "llamma-3.3-70b-versatile"  # or "gpt-4o" for the full version
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
