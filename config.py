1d0d7f359c838d865533dd911c8303796ec887c4
syedshamir <shamirraza44@gmail.com>
2026-07-05 16:16:20 +0300

    Data ingestion - Loader and Chunking

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL = "llamma-3.3-70b-versatile"  # or "gpt-4o" for the full version
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
+    QDRANT_COLLECTION_NAME: str = os.getenv("QDRANT_COLLECTION_NAME")
+    QDRANT_API_KEY: str = os.getenv("QDRANT_API_KEY")
+    QDRANT_CLUSTER_ENDPOINT: str = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+
+settings = Settings()
